13314211ba8baa1a235253a52161cec0c35a8133
Chris Cornell <ccornell@manticoregames.com>
2020-05-04 11:13:14 -0700

    Spike drops when dead/disconnected
    This file didn't get checked in before.

--- data/Subfolders/The Spike/Scripts/Spike Weapon Script.lua
+++ data/Subfolders/The Spike/Scripts/Spike Weapon Script.lua
@@ -1,8 +1,8 @@
-local ABI = require(script:GetCustomProperty("ABI"))
-
 local propSpike_Planted = script:GetCustomProperty("Spike_Planted")
 local propBasicRifle = script:GetCustomProperty("BasicRifle")
 local propTriggerZoneManager = script:GetCustomProperty("TriggerZoneManager")
+local propPickupTrigger = script:GetCustomProperty("PickupTrigger"):WaitForObject()
+
 local tm = require(propTriggerZoneManager)
 
 local ability = script.parent
@@ -30,10 +30,72 @@ ability.executeEvent:Connect(function(ability)
 	local owner = ability.owner
 	ability.parent:Unequip()
 
-	--local newGun = World.SpawnAsset(propBasicRifle)
-	--newGun:Equip(owner)
-	
 	
 	ability.parent:Destroy()
 	
+end)
+
+
+local equipment = script.parent.parent
+
+propPickupTrigger.beginOverlapEvent:Connect(function(trigger, other)
+	if other:IsA("Player") and equipment.owner == nil then
+		equipment:Equip(other)
+		SetupPlayerListeners(equipment, other)
+	end
+end)
+
+
+
+local playerDeathListener = nil
+local playerDCListener = nil
+
+
+function SetupPlayerListeners(equipment, player)
+	CleanupPlayerListeners()
+	playerDeathListener = player.diedEvent:Connect(OnPayerDeath)
+	playerDCListener = Game.playerLeftEvent:Connect(OnPlayerDC)
+end
+
+function CleanupPlayerListeners()
+	if playerDeathListener ~= nil then
+		playerDeathListener:Disconnect()
+		playerDeathListener = nil
+	end
+	if playerDCListener ~= nil then
+		playerDCListener:Disconnect()
+		playerDCListener = nil
+	end
+end
+
+
+function OnPlayerDC(player)
+	if player == equipment.owner then
+		DropSpike()
+	end
+end
+
+function OnPayerDeath(player)
+	print("Player died, dropping spike")
+	DropSpike()
+end
+
+function DropSpike()
+ 	CleanupPlayerListeners()
+	local pos = equipment:GetWorldPosition()
+	local result = World.Raycast(pos, pos + Vector3.New(0, 0, -60000), {
+		ignorePlayers = true
+	})
+	CoreDebug.DrawLine(pos, pos + Vector3.New(0, 0, -60000), {duration = 50})
+	if result then
+		local newPos = result:GetImpactPosition()
+		equipment:SetWorldPosition(newPos)
+	end
+	equipment:Unequip()
+end
+
+Events.Connect("TeamVictory", function(team)
+	CleanupPlayerListeners()
+	equipment:Unequip()
+	equipment:Destroy()
 end)
