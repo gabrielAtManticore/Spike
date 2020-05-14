6e50f795c14f17e53d116bda30161a83ae2c8869
standardcombo <standardcombo@gmail.com>
2020-05-14 14:26:44 -0700

    Fixed a bug in the Knife where you'd be stuck in the rifle pose after dying.

--- data/Scripts/EquipmentAnimationStance.lua
+++ data/Scripts/EquipmentAnimationStance.lua
@@ -2,6 +2,22 @@
 local EQUIPMENT = script:FindAncestorByType("Equipment")
 local STANCE = script:GetCustomProperty("AnimationStance")
 
+local scheduleStance = false
+
 EQUIPMENT.equippedEvent:Connect(function(equipment)
-	equipment.owner.animationStance = STANCE
+	EQUIPMENT.owner.animationStance = STANCE
+	
+	if EQUIPMENT.owner.isDead then
+		scheduleStance = true
+	end
 end)
+
+function Tick()
+	if scheduleStance 
+	and Object.IsValid(EQUIPMENT.owner) 
+	and not EQUIPMENT.owner.isDead then
+		scheduleStance = false
+		
+		EQUIPMENT.owner.animationStance = STANCE
+	end
+end
